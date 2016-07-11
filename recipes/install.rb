c9dc71e9092ab46f206bad612848ad1aaf6b5bfd
Gregory Reburn <greburn@ebsco.com>
2016-07-11 17:36:24 +0000

    setup yum repo

--- recipes/install.rb
+++ recipes/install.rb
@@ -3,3 +3,10 @@
 # Recipe:: install
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+yum_repository 'mongodb-org-3.2' do
+  description 'MongoDB Repository'
+  baseurl 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86/'
+  gpgcheck true
+  gpgkey 'https://www.mongodb.org/static/pgp/server-3.2.asc'
+  enabled true
+  action :create
