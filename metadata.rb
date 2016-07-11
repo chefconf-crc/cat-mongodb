84ea335561a6ec5835eb721453d0a4e46f6e8e3d
Daniel Turner <dturner014@gmail.com>
2016-07-11 20:31:16 +0000

    updating metadata version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures cat-mongodb'
 long_description 'Installs/Configures cat-mongodb'
-version '0.1.0'
+version '0.1.1'
 
 depends 'yum'
