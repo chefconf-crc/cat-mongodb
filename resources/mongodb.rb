68f7c803aa1e400d0a54709b260e21cd79567cbe
Gregory Reburn <gmreburn@gmail.com>
2016-07-11 15:28:58 -0500

    bugfix

--- resources/mongodb.rb
+++ resources/mongodb.rb
@@ -1,22 +1,22 @@
 resource_name :mongodb
 
 action :install do
-  yum_repository 'mongo-org-3.2' do
+  yum_repository 'mongodb-org-3.2' do
     description 'MongoDB Repository'
-    baseurl 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86/'
+    baseurl 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/'
     gpgcheck true
     gpgkey 'https://www.mongodb.org/static/pgp/server-3.2.asc'
-    enable true
+    enabled true
     action :create
   end
  
-  yum_package 'mongodb-org' do
-    action :install
-  end
- 
-  service 'mongod' do
-    action [ :enable, :start ]
-  end
+ yum_package 'mongodb-org' do
+   action :install
+ end
+
+ service 'mongod' do
+   action [ :enable, :start ]
+ end
 end
 
 action :uninstall do
