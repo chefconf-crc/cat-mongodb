2a951d85f593fb5315d62be6c4ce30328b5f7800
Gregory Reburn <greburn@ebsco.com>
2016-07-11 17:24:26 +0000

    chef generate cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'cat-mongodb'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures cat-mongodb'
+long_description 'Installs/Configures cat-mongodb'
+version '0.1.0'
