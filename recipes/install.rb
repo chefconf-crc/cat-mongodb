003409cd490beeb1d33b8caaa41d83da91011bec
OH YEAH <sumac@ip-10-0-0-116.ec2.internal>
2016-07-11 20:19:04 +0000

    Changing Install.rb

--- recipes/install.rb
+++ recipes/install.rb
@@ -4,24 +4,6 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-yum_repository 'mongodb-org-3.2' do
-  description 'MongoDB Repository'
-  baseurl 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/'
-  gpgcheck true
-  gpgkey 'https://www.mongodb.org/static/pgp/server-3.2.asc'
-  enabled true
-  action :create
-end
- 
-yum_package 'mongodb-org' do
+mongodb 'install_mongodb' do
   action :install
-end
-
-service 'mongod' do
-  action [ :enable, :start ]
-end
-
-httpd 'build website' do
-  homepage '<h1>Welcome to CAT Team Website!<h1>'
-  action :create
 end
